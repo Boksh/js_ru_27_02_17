aef21e6746e1e363e7d2fc9e84a3f3b29f65956d
Roman <kaboom.ua@gmail.com>
2017-03-22 22:50:34 +0200

    Update uuid.js

--- src/middlewares/uuid.js
+++ src/middlewares/uuid.js
@@ -6,6 +6,7 @@ export default store => next => action => {
     const { type, payload } = action
 
     switch (type) {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
         case ADD_COMMENTS: {
             next({
                 ...action,
